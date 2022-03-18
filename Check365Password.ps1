60312b71aeb18cd1249e319a4b3f97a3ae4b0a4c
Runard <40338703+alexandremottier@users.noreply.github.com>
2022-03-18 13:45:40 +0100

    v1.1
    v1.1

---
+++ Check365Password.ps1
@@ -0,0 +1,15 @@
+﻿Write-Host -ForegroundColor Yellow "...::: Outil de vérification de mot de passe Microsoft 365 :::..."
+while(0 -lt 1){
+$Credential = Get-Credential
+Write-Host -ForegroundColor Yellow "Test de connexion en cours... " -NoNewline
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $Credential -Authentication Basic -AllowRedirection
+Import-PSSession $Session | Out-Null
+
+if ($Error.Count -eq 0) {
+    Write-Host -ForegroundColor Green "Connexion OK !"
+}else{
+    Write-Host -ForegroundColor Red "Erreur de connexion ! Le mot de passe est-il correct ?"
+}
+
+Remove-PSSession $Session
+}
